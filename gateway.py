393ef8fa9a3bcde5c422c04ee7d8b667c6f03681
Sagiya54 <al18053@shibaura-it.ac.jp>
2021-02-19 10:31:56 +0900

    ゲートウェイ完成

---
+++ gateway.py
@@ -0,0 +1,13 @@
+import serial
+
+ser = serial.serial('/dev/ttyACM0',9600,timeout=10)
+
+is_first = True
+while True:
+    lux = ser.readline().decode('utf-8')
+    if is_first:
+        is_first = False
+    else:
+        print(lux)
+
+ser.close()
